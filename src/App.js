00bb97451641807b748e332faad1b62341df2154
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-26 11:27:43 +0100

    Added the css file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './assets/css/main.css';
+import Layout from '../hoc/layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello
+        <Layout />
       </div>
     );
   }
