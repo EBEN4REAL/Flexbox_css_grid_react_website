9aa0c86f00992fae7c4266668e8bb67da8045827
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-26 11:48:48 +0100

    Added the css file

--- src/hoc/layout/Layout.js
+++ src/hoc/layout/Layout.js
@@ -1,17 +1,15 @@
 import React, {Component}  from 'react';
 import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
 
-class Card extends Component {
+class Layout extends Component {
     render(){
         return (
             <div>
                 <Header />
                     {this.props.children}
-                <Footer />
             </div>
             
         )
     }
 }
-export default Card
+export default Layout;
