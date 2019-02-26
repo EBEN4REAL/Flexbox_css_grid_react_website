b4831dd74c3f54d14208da7857c680cae0e3f704
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-26 11:45:01 +0100

    Added the css file

--- src/hoc/layout/Layout.js
+++ src/hoc/layout/Layout.js
@@ -1,10 +1,16 @@
 import React, {Component}  from 'react';
 import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 
 class Card extends Component {
     render(){
         return (
-            <Header />
+            <div>
+                <Header />
+                    {this.props.children}
+                <Footer />
+            </div>
+            
         )
     }
 }
