cc6e927d6c5e8e8fa0b5c6fbad36f014ce39db63
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-02-26 11:14:10 +0100

    Added the css file

---
+++ components/Header/Header.js
@@ -0,0 +1,15 @@
+ import React , {Component} from 'reaact';
+
+ class Header extends Component {
+
+     render(){
+         return  (
+            <div>
+
+            </div>
+         )
+     }
+
+ }
+
+ export default Header;
